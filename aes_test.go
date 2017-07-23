111b6bb5cd8f232a8d07fabf0f9d936cb4664ed6
Michael McLoughlin <mmcloughlin@gmail.com>
2017-07-23 11:09:48 -0700

    move function declarations

--- aes_test.go
+++ aes_test.go
@@ -6,16 +6,6 @@ import (
 	"testing"
 )
 
-func expandKeyAsm(nr int, key *byte, enc *uint32, dec *uint32)
-func encryptBlockAsm(nr int, xk *uint32, dst, src *byte)
-func encryptBlocks2Asm(nr int, xk *uint32, dst, src *byte)
-func encryptBlocks4Asm(nr int, xk *uint32, dst, src *byte)
-func encryptBlocks6Asm(nr int, xk *uint32, dst, src *byte)
-func encryptBlocks8Asm(nr int, xk *uint32, dst, src *byte)
-func encryptBlocks10Asm(nr int, xk *uint32, dst, src *byte)
-func encryptBlocks12Asm(nr int, xk *uint32, dst, src *byte)
-func encryptBlocks14Asm(nr int, xk *uint32, dst, src *byte)
-
 type Encryptor func(nr int, xk *uint32, dst, src *byte)
 
 var v = struct {
